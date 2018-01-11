c54a64c8fb2b674d961237e815d588c45e0b2c3d
Ramansh Shukla <ramansh.shukla@blueoptima.com>
2018-01-11 11:38:19 +0530

    writing and reading from json done

---
+++ src/com/company/ForCreatingSingleObject.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.List;
+
+public class ForCreatingSingleObject {
+    List<Info> empList;
+
+    public List<Info> getEmpList() {
+        return empList;
+    }
+
+    public void setEmpList(List<Info> empList) {
+        this.empList = empList;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import org.omg.CORBA.INTERNAL;
+
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        //arraylist to store info of 5 employees
+        //arraylist to store info of 5 employees.
         ArrayList<Info> allData = new ArrayList<Info>();
 
         //addition by 1 to allData
@@ -32,6 +39,58 @@ public class Main {
         data = new Info(5,"David Gorton","Operations",9999,curr);
         allData.add(data);
 
-        
+
+        String json = new Gson().toJson(allData);
+        System.out.println(json+"\nwrite to json");
+
+        writeJsonToFile(json);
+        problem2();
+    }
+
+    public static void problem2() {
+        Object jsonData = readJsonFromFile("jsonFile");
+        //System.out.println(jsonData);
+
+        Gson gsonObject = new Gson();
+        List<Info> temp = gsonObject.fromJson((String) jsonData, List.class);
+        System.out.println(temp+"\nread from json");
+    }
+
+
+    public static void writeJsonToFile(String json) {
+        File file = new File("jsonFile");
+
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            ObjectOutputStream s = new ObjectOutputStream(f);
+            s.writeObject(json);
+            s.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Object readJsonFromFile(String fileName) {
+        Object data=null;
+        try{
+            FileInputStream f = new FileInputStream(fileName);
+            ObjectInputStream s = new ObjectInputStream(f);
+            data=s.readObject();
+            s.close();
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return data;
     }
 }
+
+
